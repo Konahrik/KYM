5a061d659a154c405353f1c6d1977473af3e87ae
Konahrik <olesia_96@mail.ru>
2016-03-13 19:41:48 +0300

    VkVideo

---
+++ KnowYourMusic/Details/VkVideo.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowYourMusic.Details
+{
+    public class VkVideo
+    {
+        public List<VideoResponse> response { get; set; }
+    }
+
+    public class VideoResponse
+    {
+        [JsonProperty("title")]
+        public string VideoTitle { get; set; }
+        [JsonConverter(typeof(SecondsToStringConverter))]
+        [JsonProperty("duration")]
+        public string VideoDuration { get; set; }
+        [JsonProperty("player")]
+        public string VideoPlayer { get; set; }
+    }
+}
