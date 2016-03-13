2abc08c9023fda337da515a2388a15649763b5d1
well1996@bk.ru <qwedfgbnm>
2016-03-13 22:23:51 +0300

    general and audio tests

---
+++ KnowYourMusic.Test/AudioTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KnowYourMusic.Features;
+
+namespace KnowYourMusic.Test
+{
+    [TestClass]
+    public class AudioTest
+    {
+        [TestMethod]
+        public void VK_Search_Audio_Request_Failed_Auth()
+        {
+            var result = Audio.LoadAudio("durov");
+
+            Assert.AreEqual(null, result);
+        }
+
+        [TestMethod]
+        public void VK_Load_Audio_Request_Exception_Failed_Auth()
+        {
+            var result = Audio.SearchAudio("OST");
+
+            Assert.AreEqual(null, result);
+        }
+    }
+}
---
+++ KnowYourMusic.Test/GeneralTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KnowYourMusic.Features;
+using Newtonsoft.Json;
+using KnowYourMusic.Details;
+
+namespace KnowYourMusic.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void VK_Request()
+        {
+            string result = General.VkRequest("https://api.vk.com/method/users.get?uids=durov");
+
+            Assert.AreEqual("{\"response\":[{\"uid\":1,\"first_name\":\"Павел\",\"last_name\":\"Дуров\"}]}", result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UriFormatException))]
+        public void VK_Request_Exception_Is_Thrown()
+        {
+            string result = General.VkRequest("Wrong String!");
+        }
+
+        [TestMethod]
+        public void Correct_Users_Info()
+        {
+            var result = General.GetUsersInfo("durov");
+
+            var expected = JsonConvert.DeserializeObject<VkUsers>(@"{""response"":[{""uid"":1,""first_name"":""Павел"",""last_name"":""Дуров""}]}");
+            Assert.AreEqual(expected.response[0].uid, result.response[0].uid);
+        }
+    }
+}
